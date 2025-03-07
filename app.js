e6d8f47b75730d9e15b0dc2ddc52d9271c4740ac
Udin Mustofa <muchammad.udin.mustofa@mail.ugm.ac.id>
2025-03-07 22:14:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 3000; // Gunakan PORT dari environment
 
 app.use(express.static('public'));
 
-//route
+// Routes
 app.get('/', (req, res) => {
-  res.sendFile('./pages/index.html', {root: __dirname});
+  res.sendFile('./pages/index.html', { root: __dirname });
 });
 app.get('/contact', (req, res) => {
-  res.sendFile('./pages/contact.html', {root: __dirname});
+  res.sendFile('./pages/contact.html', { root: __dirname });
 });
 app.get('/profiles', (req, res) => {
-  res.sendFile('./pages/profiles.html', {root: __dirname});
-})
+  res.sendFile('./pages/profiles.html', { root: __dirname });
+});
 app.get('/skills', (req, res) => {
-  res.sendFile('./pages/skills.html', {root: __dirname});
-})
+  res.sendFile('./pages/skills.html', { root: __dirname });
+});
 app.get('/experience', (req, res) => {
-  res.sendFile('./pages/experience.html', {root: __dirname});
-})
-app.use('/', (req, res) => {
-    res.send('<h1>404</h1>');
-}) //dijalankan terus, digunakan untuk link yg tidak ada saja
+  res.sendFile('./pages/experience.html', { root: __dirname });
+});
 
+// Middleware 404 harus di akhir
+app.use((req, res) => {
+  res.status(404).send('<h1>404 - Page Not Found</h1>');
+});
 
 app.listen(port, () => {
-  console.log(`Server berjalan pada port ${port}`);
-})
+  console.log(`Server berjalan di port ${port}`);
+});
